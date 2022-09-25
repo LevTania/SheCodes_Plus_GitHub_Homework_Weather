2700329f61d3a2eb7517c3d78f044ea0495b88f7
LevTania <tanyalevandovska@gmail.com>
2022-09-25 18:23:59 +0300

    Added js

---
+++ index.js
@@ -0,0 +1,38 @@
+//date
+function formatDate(now) {
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = now.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[dayIndex];
+  return `${day}., <br/> ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+//city
+function weather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+//search city
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let city = cityInput.value;
+  let key = "49b631c45785fe73d2a88477803dea22";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  axios.get(url).then(weather);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
