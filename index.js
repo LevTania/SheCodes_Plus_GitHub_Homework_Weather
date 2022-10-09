3ce54a4b66781a756962fcf6f0488707dfefda73
LevTania <tanyalevandovska@gmail.com>
2022-10-09 15:14:43 +0300

    Weather Forecast / API

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ function formatDate(now) {
 let dateElement = document.querySelector("#time");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
+
+//Forecast prediction (coordinates)
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let key = "49b631c45785fe73d2a88477803dea22";
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
+  axios.get(url).then(displayForecast);
+}
+
 //city, icon, temperature
 function weather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -40,6 +49,7 @@ function weather(response) {
   document.querySelector("#temperature").innerHTML = `${Math.round(
     (celsiusTemperature = response.data.main.temp)
   )}`;
+  getForecast(response.data.coord);
 }
 //search city
 function search(event) {
@@ -73,7 +83,8 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 //Column repeat
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let day = [
@@ -100,6 +111,4 @@ function displayForecast() {
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
-displayForecast();
